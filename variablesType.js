8aa28782d7e046a58aa6530a173c0e68677f44d9
HafssaBoukniter <hafssaboukniter2002@gmail.com>
2026-09-01 15:52:22 +0200

    Variables & Type

---
+++ variablesType.js
@@ -0,0 +1,125 @@
+const prompt = require('prompt-sync')();
+/*
+//challenge 1
+let nom = prompt('Saisir votre nom :');
+let prenom = prompt('Saisir votre prenom :');
+let age = prompt('Saisir votre age :');
+let sex = prompt('Saisir votre sexe(M/F) :');
+let email = prompt('Saisir votre e-mail :');
+
+console.log(`Voici votre votre informations :
+    Nom : ${nom}
+    Prenom : ${prenom}
+    Age : ${age}
+    Sex : ${sex}
+    E-mail : ${email}`);
+
+
+//challenge 2
+let tempC = parseFloat(prompt('Saisir votre température :'));
+let tempK = tempC + 273.15;
+
+console.log(`Votre ${tempC} °C correspond à ${tempK} K`);
+
+//challenge 3
+let disKelometres = prompt('Saisir votre distance en kilomètres :');
+let yard = disKelometres * 1093.61;
+
+console.log(`${disKelometres} Km est transforme en ${yard} yards`);
+
+//challenge 4
+let kmHeure = prompt('Saisir la vitesse en kilomètres par heure(km/h) :');
+let KmSeconde = kmHeure * 0.27778;
+
+console.log(`La vitesse en kilomètres par heure (km/h) va transforme en mètres par seconde (m/s) : ${KmSeconde}`);
+
+//challenge 5
+let temperatureC = prompt('Saisir la température en Celsius :');
+if(temperatureC < 0){
+    console.log(`Votre température est Solide`);
+}else if (temperatureC >= 0 && temperatureC < 100) {
+    console.log(`Votre température est Liquide`);
+}else {
+    console.log(`Votre température est Gaz`);
+}
+
+//challenge 6
+let a = parseInt(prompt('Saisir le premier Nombre a :'));
+let b = parseInt(prompt('Saisir le premier Nombre b :'));
+
+let addition = a + b;
+let soustraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log(`Voici votre Calculs :
+    Addition(a+b) = ${addition}
+    Soustraction(a-b) = ${soustraction}
+    Multiplication(axb) = ${multiplication}
+    division(a/b) = ${division}`);
+
+//challenge 7
+let numb1 = prompt('Donner votre 1er nombre :');
+let numb2 = prompt('Donner votre 2ème nombre :');
+let numb3 = prompt('Donner votre 3ème nombre :');
+
+let moyennePonderee = ((numb1*2) + (numb2*3) + (numb3*5))/3;
+
+console.log(`La moyenne pondérée de trois nombres donnés est ${moyennePonderee.toFixed(2)}`);
+
+//challenge 8
+let number1 = prompt('Donner votre 1er nombre :');
+let number2 = prompt('Donner votre 2ème nombre :');
+let number3 = prompt('Donner votre 3ème nombre :');
+
+let moyenneGeometrique = (number1 * number2 * number3)**(1/3);
+
+console.log(`La moyenne géométrique de trois nombres est : ${moyenneGeometrique.toFixed(2)}`);
+
+//challenge 9 
+let x1 = prompt('Saisir la premier point de X :');
+let x2 = prompt('Saisir la dexieme point de X :');
+let y1 = prompt('Saisir la premier point de Y :');
+let y2 = prompt('Saisir la dexieme point de Y :');
+let z1 = prompt('Saisir la premier point de Z :');
+let z2 = prompt('Saisir la dexieme point de Z :');
+
+let distance = Math.sqrt((x2 - x1)**2 + (y2- y1)**2 + (z2-z1)**2);
+
+console.log(`La distance entre deux points donnés dans un espace 3D est : ${distance.toFixed(2)}`);
+
+//challenge 10
+const pi = 3.14;
+let r = prompt('Saisir le rayon de sphère :');
+
+let volume = (4/3) * pi *r**3;
+
+console.log(`Le volume d'une sphère est ${volume.toFixed(2)}`);
+
+//challenge 11
+let long = prompt('Saisir la longueur de rectangle :');
+let larg = prompt('Saisir la largeur de rectangle :');
+
+let surface = long * larg;
+
+console.log(`La surface d'un rectangle est : ${surface}`);
+
+//challenge 12
+let chiffre = prompt('Saisir un nombre entier à quatre chiffres :');
+
+let a = chiffre % 10;
+chiffre = parseInt(chiffre / 10);
+let b = chiffre % 10;
+chiffre = parseInt(chiffre / 10);
+let c = chiffre % 10;
+chiffre = parseInt(chiffre / 10);
+let d = chiffre % 10;
+
+console.log(`L'inverse de nombre entier à quatre chiffres est : ${a}${b}${c}${d}`);
+*/
+//challenge 13
+//let num = prompt('Saisir un nombre entier :');
+//let numBinaire = num;
+//let numHexadecimal = num;
+
+//console.log(`La valeur de ${num} équivalente en binaire est ${numBinaire} et en hexadécimal est ${numHexadecimal}`);
