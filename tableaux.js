358c96a3b5e2e63ce05be6e5f594b4ddece56adf
HafssaBoukniter <hafssaboukniter2002@gmail.com>
2026-09-05 17:51:12 +0200

    Tableaux

---
+++ tableaux.js
@@ -0,0 +1,98 @@
+const prompt = require('prompt-sync')();
+/*
+//challenge 1
+let tab1 = [1, 2, 3, 4, 5];
+
+for(let i = 0;i < tab1.length;i++){
+    console.log(tab1[i]);
+}
+
+//challeng 2
+let tab2 = [];
+let t1 = prompt("Saisir la taille de Tableaux :");
+
+for(let i = 0;i < t1;i++){
+    tab2[i] = prompt(`Saisir l'element ${i+1} de tableau :`);
+}
+
+console.log(`Voici les elements de tableu ${tab2.join(" ")}`);
+
+//challenge 3
+let t2 = prompt("Saisir la taille de Tableaux :");
+let tab3 = [];
+
+let s = 0;
+
+for(let i = 0;i < t2;i++){
+    tab3[i] = Number(prompt(`Saisir l'element ${i+1} de tableau :`));
+    s+=tab3[i];
+}
+
+console.log(`La somme des elements de tableau est ${s}`);
+
+//challenge 4
+let t3 = prompt("Saisir la taille de Tableaux :");
+let tab4 = [];
+
+for(let i = 0;i < t3;i++){
+    tab4[i] = Number(prompt(`Saisir l'element ${i+1} de tableau :`));
+}
+
+let max = tab4[0];
+for(let i = 1;i < t3;i++){
+    if(tab4[i] > max){
+        max = tab4[i];
+    }
+}
+
+console.log(`Le plus grand élément dans un tableau est ${max}`);
+
+//challenge 5
+let t4 = prompt("Saisir la taille de Tableaux :");
+let tab5 = [];
+
+for(let i = 0;i < t4;i++){
+    tab5[i] = Number(prompt(`Saisir l'element ${i+1} de tableau :`));
+}
+
+let min = tab5[0];
+for(let i = 1;i < t4;i++){
+    if(tab5[i] < min){
+        min = tab5[i];
+    }
+}
+
+console.log(`Le plus petit élément dans un tableau est ${min}`);
+
+//challenge 6
+let t5 = prompt("Saisir la taille de Tableaux :");
+let tab6 = [];
+
+for(let i = 0;i < t5;i++){
+    tab6[i] = Number(prompt(`Saisir l'element ${i+1} de tableau :`));
+}
+let f = Number(prompt(`Saisir le facteur de multiplication :`));
+ 
+for(let i = 0;i < t5;i++){
+    tab6[i] *= f;
+}
+
+console.log(`Le tableau multiplier par ${f} est `,tab6);
+
+//challenge 7
+let elements = prompt("Saisir un tableau d'entiers comme 1 2 3 :");
+
+let tab7 = elements.split(" ").map(Number);
+
+console.log(tab7.sort());
+*/
+//challenge 8
+let tab8 = [1,2,3,4];
+let tab8Copie = tab8.slice();
+let tab8Verification = tab8.every((elm,index) => elm === tab8Copie[index]);
+
+console.log(tab8);
+console.log(tab8Copie);
+console.log(tab8Verification);
+
+//challege 9 
