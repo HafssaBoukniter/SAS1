1b0e66d3e1cb7a41707bd2c3cf2e732a3e9d9fee
HafssaBoukniter <hafssaboukniter2002@gmail.com>
2026-09-07 18:01:55 +0200

    Objets

--- objets.js
+++ objets.js
@@ -115,21 +115,52 @@ const menu = {
     },
 };
 
-let choix;
-do{
-    choix = Number(prompt(`    Menu    
+function menuSaisi(){
+    console.log((`    Menu    
     1-Home 
     2-Products
     3-Services 
     `));
+}
 
-    switch(choix){
-        case 1 : let home =prompt(`    Home    
+function homeMenu(){
+    console.log((`    Home    
         1-Welcome 
         2-About-us
         3-Contact 
         #-Back
-        `);
+        `));
+}
+
+function produitsMenu(){
+    console.log((`    Products    
+        1-New arrivals 
+        2-Best sellers
+        3-Categories 
+        #-Back
+    `));
+}
+
+function servicesMenu(){
+    console.log((`    Products    
+        1-Consulting
+        2-Support
+        3-FAQ
+        #-Back
+    `));
+}
+
+let choix = 0;
+do{
+
+    switch(choix){
+        case 0 :
+                menuSaisi();
+                choix = Number(prompt(`Saisir votre choix :`));
+                break;
+        case 1 :
+            homeMenu();
+            let home =prompt();
             switch(home){
                 case '1' : console.log(menu.home.welcome);
                 break;
@@ -137,16 +168,13 @@ do{
                 break;
                 case '3' : console.log(menu.home.contact);
                 break;
-                case '#' : 
+                case '#' : choix = 0;
                 break;
-            }
+            };
         break;
-        case 2 : let produits =prompt(`    Products    
-        1-New arrivals 
-        2-Best sellers
-        3-Categories 
-        #-Back
-        `);
+        case 2 :
+            produitsMenu();
+            let produits =prompt();
             switch(produits){
                 case '1' : console.log(menu.products.newArrivals);
                 break;
@@ -154,16 +182,13 @@ do{
                 break;
                 case '3' : console.log(menu.products.categories);
                 break;
-                case '#' : 
+                case '#' : choix = 0;
                 break;
-            }
+            };
         break;
-        case 3 : let services =prompt(`    Products    
-        1-Consulting
-        2-Support
-        3-FAQ
-        #-Back
-        `);
+        case 3 : 
+            servicesMenu();
+            let services =prompt();
             switch(services){
                 case '1' : console.log(menu.services.consulting);
                 break;
@@ -171,9 +196,9 @@ do{
                 break;
                 case '3' : console.log(menu.services.faq);
                 break;
-                case '#' : 
+                case '#' : choix = 0;
                 break;
-            }
+            };
         break;
     };
 }while(choix !== 4);
